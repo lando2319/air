4c74b9c2ff90f43e40be068dfefea36dd8aa68ee
Jeff Cohen <jeff@codeacademy.org>
2013-02-22 09:32:29 -0600

    Start working on user story #2

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_name(params[:name])
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "Welcome back, #{user.name}"
+      return
+    end
+    redirect_to sign_in_url, notice: "Nice try!"
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 OneWayAir::Application.routes.draw do
 
+  get "sessions/new", :as => :sign_in
+
+  post "sessions/create"
+
+  get "sessions/destroy"
+
+  resources :users
+
+
   root to: 'flights#index'
 end
